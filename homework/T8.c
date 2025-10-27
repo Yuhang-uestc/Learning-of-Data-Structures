5bdde46ac6a0402e40b6f4d806150f837ce33650
Yuhang-uestc <2184746814@qq.com>
2025-10-27 23:15:11 +0800

    更新

--- homework/T8.c
+++ homework/T8.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stdlib>
+#include<stdlib.h>
 
 #define MAX_SIZE 100
 typedef struct{
@@ -15,4 +15,107 @@ void InitStack(Stack *s)
 int IsEmpty(Stack *s)
 {
 	return s->top == -1;
+}
+int IsFull(Stack *s)
+{
+    return s->top == MAX_SIZE - 1;
+}
+int Push(Stack *s,int value)
+{
+	if(IsFull(s))
+	{
+		printf("栈满！\n");
+		return 0;
+	}
+	s->data[++(s->top)] = value;
+	return 1;
+}
+int Pop(Stack *s)
+{
+	if(IsEmpty(s))
+	{
+		printf("栈空！\n");
+		return -1;
+	}
+	return s->data[(s->top)--];
+}
+int GetTop(Stack *s)
+{
+	if(IsEmpty(s))
+	{
+		printf("栈空！\n");
+		return -1;
+	}
+	return s->data[s->top ];
+}
+
+typedef struct{
+	Stack stack_in;
+	Stack stack_out;
+}Que;
+
+void InitQue(Que *q)
+{
+	InitStack(&q->stack_in);
+	InitStack(&q->stack_out);
+}
+int EnQue(Que *q,int value)
+{
+	return Push(&q->stack_in,value);
+}
+int DeQue(Que *q)
+{
+	if(IsEmpty(&q->stack_out))
+	{
+		while(!IsEmpty(&q->stack_in))
+		{
+			int val = Pop(&q->stack_in);
+			Push(&q->stack_out,val);
+		}
+	}
+	if(IsEmpty(&q->stack_out))
+	{
+		printf("队列为空，无法出队！\n");
+		return -1;
+	}
+	return Pop(&q->stack_out);
+}
+int Front(Que *q)
+{
+	if(IsEmpty(&q->stack_out))
+	{
+		while(!IsEmpty(&q->stack_in))
+		{
+		int val = Pop(&q->stack_in);
+        Push(&q->stack_out, val);
+		}
+	}
+	if (IsEmpty(&q->stack_out)) 
+	{
+        printf("队列为空，无队首元素！\n");
+        return -1;
+    }
+    return GetTop(&q->stack_out);
+}
+
+int main()
+{
+	Que q;
+	InitQue(&q);
+	
+	EnQue(&q,1);
+	EnQue(&q,2);
+	EnQue(&q,3);
+	printf("队首元素：%d\n",Front(&q));
+	printf("出队元素：%d\n",DeQue(&q));
+	printf("队首元素：%d\n",Front(&q));
+	
+	EnQue(&q, 4);
+    printf("出队元素：%d\n", DeQue(&q));
+    printf("出队元素：%d\n", DeQue(&q));
+    printf("队首元素：%d\n", Front(&q));
+    printf("出队元素：%d\n", DeQue(&q));
+    printf("出队元素：%d\n", DeQue(&q));
+    
+    return 0;
 }
